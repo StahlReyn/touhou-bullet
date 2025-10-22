23e7f8c296233e275da6e0caff7eee6d4b4049e6
StahlReyn <reynstahl@gmail.com>
2025-10-21 22:54:38 -0400

    Boss Aura shaders

---
+++ data/enemies/bosses/boss_aura.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+enum Status {
+	HIDE,
+	START,
+	SHOW,
+	END
+}
+
+@onready var viewport_size: Vector2 = get_viewport().size
+@onready var distort := $Distort
+@onready var circle := $Circle
+
+@onready var distort_shader: ShaderMaterial = distort.material
+@onready var circle_shader: ShaderMaterial = circle.material
+
+@onready var main_radius: float = distort_shader.get_shader_parameter("radius")
+@onready var main_alpha: float = circle_shader.get_shader_parameter("alpha")
+
+var alpha: float = 0.0
+var radius: float = 0.0
+var x_rot: float = 0.0
+var y_rot: float = 0.0
+var time: float = 0.0
+
+var status: Status = Status.HIDE
+
+func _ready() -> void:
+	start()
+
+func _physics_process(delta: float) -> void:
+	match status:
+		Status.START:
+			alpha += delta
+			radius = min(radius + delta, main_radius)
+			if alpha >= main_alpha:
+				alpha = main_alpha
+				status = Status.SHOW
+		Status.END:
+			alpha -= delta
+			radius = max(radius - delta, 0.0)
+			if alpha <= 0.0:
+				alpha = 0.0
+				status = Status.HIDE
+			
+	rotation = time * -0.7
+	x_rot = sin(time * 0.1) * 5
+	y_rot = cos(time * 0.2) * 40
+	
+	circle_shader.set_shader_parameter("x_rot", x_rot)
+	circle_shader.set_shader_parameter("y_rot", y_rot)
+	circle_shader.set_shader_parameter("alpha", alpha)
+	
+	distort_shader.set_shader_parameter("center", global_position / viewport_size)
+	distort_shader.set_shader_parameter("radius", radius)
+	
+	time += delta
+
+func start() -> void:
+	status = Status.START
+	circle_shader.set_shader_parameter("alpha", alpha)
+	distort_shader.set_shader_parameter("center", global_position / viewport_size)
+	distort_shader.set_shader_parameter("radius", radius)
--- scripts/entities/bullet.gd
+++ scripts/entities/bullet.gd
@@ -6,27 +6,30 @@ extends Entity
 @export var main_sprite: Sprite2D
 
 var do_spawn_effect: bool = true ## This will make bullet do slight fade and scale-in transition
-var prev_scale: Vector2
-var prev_alpha: float
+var main_scale: Vector2
+var main_alpha: float
+var fading_in: bool = false
 
 const INIT_SPAWN_SCALE := 3.0
-const SPAWN_SCALE_SPEED := 20.0
-const SPAWN_OPACITY_SPEED := 5.0
+const SPAWN_SCALE_SPEED := 10.0
+const SPAWN_OPACITY_SPEED := 10.0
 	
 func _ready() -> void:
 	super()
 	add_to_group("bullet")
-	#prev_alpha = main_sprite.modulate.a
-	#prev_scale = main_sprite.scale
-	#if do_spawn_effect:
-		#main_sprite.modulate.a = 0
-		#main_sprite.scale = prev_scale * INIT_SPAWN_SCALE
+	if main_sprite != null and do_spawn_effect:
+		fading_in = true
+		main_alpha = main_sprite.modulate.a
+		main_scale = main_sprite.scale
+		main_sprite.modulate.a = 0
+		main_sprite.scale = main_scale * INIT_SPAWN_SCALE
 
 func _physics_process(delta: float) -> void:
-	#if do_spawn_effect:
-		#main_sprite.modulate.a = MathUtils.lerp_smooth(main_sprite.modulate.a, prev_alpha, SPAWN_OPACITY_SPEED, delta)
-		#main_sprite.scale = MathUtils.lerp_smooth(main_sprite.scale, prev_scale, SPAWN_SCALE_SPEED, delta)
-	pass
+	if fading_in:
+		main_sprite.modulate.a = MathUtils.lerp_smooth(main_sprite.modulate.a, main_alpha, SPAWN_OPACITY_SPEED, delta)
+		main_sprite.scale = MathUtils.lerp_smooth(main_sprite.scale, main_scale, SPAWN_SCALE_SPEED, delta)
+		if main_sprite.scale == main_scale and main_sprite.modulate.a == main_alpha:
+			fading_in = false
 
 func sprite_frame_x(index: int):
 	main_sprite.frame_coords.x = index
