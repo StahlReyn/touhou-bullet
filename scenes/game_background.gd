cae9bd13d138fc3edcc320df4c74b76a32c1e756
StahlReyn <reynstahl@gmail.com>
2025-10-22 18:56:08 -0400

    Scene transition with curve

--- scenes/game_background.gd
+++ scenes/game_background.gd
@@ -1,35 +1,31 @@
 class_name GameBackground
 extends PanelContainer
 
-@onready var color_overlay: ColorRect = $Overlay
 @onready var stage_viewport: SubViewport = $BackgroundViewport/StageViewport
+@onready var animation: AnimationPlayer = $AnimationPlayer
 
-var transitioning: bool = false
-var fade_speed: float = 3.0
+@export var fade_curve: Curve
+
 var next_scene: PackedScene
 
+var progress: float = 0.0
+
 func _ready() -> void:
-	color_overlay.color.a = 0.0
+	animation.play("RESET")
+	pass
 
 func _physics_process(delta: float) -> void:
-	if transitioning:
-		color_overlay.color.a += fade_speed * delta
-		if color_overlay.color.a >= 1.0:
-			color_overlay.color.a = 1.0
-			transitioning = false
-			set_stage_scene(next_scene)
-	else:
-		if color_overlay.color.a > 0.0:
-			color_overlay.color.a -= fade_speed * delta
-			if color_overlay.color.a <= 0.0:
-				color_overlay.color.a = 0.0
+	pass
 	
 func transition_stage_scene(scene: PackedScene) -> void:
 	next_scene = scene
-	transitioning = true
+	animation.play("transition")
 	
 func set_stage_scene(scene: PackedScene) -> void:
 	for n in stage_viewport.get_children():
 		stage_viewport.remove_child(n)
 		n.queue_free() 
 	stage_viewport.add_child(scene.instantiate())
+
+func set_next_scene() -> void:
+	set_stage_scene(next_scene)
