f0ad2a22a1d84fa94f6c4787cab26448f8bc40fc
StahlReyn <reynstahl@gmail.com>
2025-10-23 14:57:00 -0400

    pause menu use signal

--- scenes/gameplay/pause_menu.gd
+++ scenes/gameplay/pause_menu.gd
@@ -11,25 +11,14 @@ func _ready() -> void:
 	visible = true
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("back") and not get_tree().paused:
-		selection_list.reset_display()
-		get_tree().paused = true
-	
-	if get_tree().paused:
-		if Input.is_action_just_pressed("shoot"):
-			match selection_list.cur_selection:
-				0: # RETURN
-					option_return()
-				1: # RETRY
-					option_retry()
-				2: # QUIT
-					option_quit()
-					
-		if Input.is_action_just_pressed("bomb"):
+	if get_tree().paused:		
+		if Input.is_action_just_pressed("bomb") or Input.is_action_just_pressed("back"):
 			option_return()
-		
 		modulate.a = MathUtils.lerp_smooth(modulate.a, 1.0, 100, delta)
 	else:
+		if Input.is_action_just_pressed("back"):
+			selection_list.reset_display()
+			get_tree().paused = true
 		modulate.a = MathUtils.lerp_smooth(modulate.a, 0.0, 100, delta)
 
 func option_return():
@@ -49,3 +38,13 @@ func option_quit():
 	await get_tree().create_timer(0.2).timeout
 	get_tree().paused = false
 	SceneManager.goto_scene(SceneManager.SCENE_MAIN_MENU)
+
+
+func _on_selection_list_selected(index: int) -> void:
+	match index:
+		0: # RETURN
+			option_return()
+		1: # RETRY
+			option_retry()
+		2: # QUIT
+			option_quit()
