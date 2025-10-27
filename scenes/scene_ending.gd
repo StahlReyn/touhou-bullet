4d9356b50fd50e9dc956c358c9030c88911e26ce
StahlReyn <reynstahl@gmail.com>
2025-10-26 23:58:05 -0400

    Ending Image fire writing

--- scenes/scene_ending.gd
+++ scenes/scene_ending.gd
@@ -6,7 +6,7 @@ extends Node2D
 @onready var image_container: Node2D = $ImageContainer
 @onready var text_label: Label = $TextPanel/MarginContainer/Text
 
-@export_multiline var texts: Array[String]
+@export var texts: Array[String]
 
 var can_input: bool = false
 var image_index: int = 0
@@ -16,14 +16,12 @@ func _ready() -> void:
 	get_tree().paused = false
 	print(image_container.get_child_count())
 	
-	for node: Node2D in image_container.get_children():
+	for node: CanvasItem in image_container.get_children():
 		node.modulate.a = 0
 	
 	input_delay.start()
 
 func _physics_process(delta: float) -> void:
-	if tween and text_label.visible_ratio >= 1:
-		tween.kill() # Finished tween
 	if can_input:
 		if Input.is_action_just_pressed("shoot") or Input.is_action_just_pressed("bomb"):
 			if tween and tween.is_valid(): # Force finish animation first
@@ -40,11 +38,11 @@ func has_next_image() -> bool:
 func next_image() -> void:
 	print("Next Image")
 	tween = create_tween()
-	var node: Node2D = image_container.get_child(image_index)
-	text_label.visible_characters = 0
-	text_label.text = texts[image_index]
+	var node: CanvasItem = image_container.get_child(image_index)
+	text_label.visible_ratio = 0
+	text_label.text = tr(texts[image_index])
 	tween.tween_property(node, "modulate", Color.WHITE, 0.4)
-	tween.tween_property(text_label, "visible_characters", 1000, 8.0)
+	tween.tween_property(text_label, "visible_ratio", 1, 2.0)
 	image_index += 1
 
 func _on_timer_timeout() -> void:
